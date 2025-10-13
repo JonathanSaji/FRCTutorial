a109c0dc02173dbeec2acebb7b9efb5786bdf5e6
Jonathan Saji <jonathansaji2009@gmail.com>
2025-10-13 18:43:04 -0400

    generic tank drive add

---
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -0,0 +1,45 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DrivetrainSubsystem extends SubsystemBase {
+  WPI_TalonRX leftFront;
+  WPI_TalonRX leftBack;
+  WPI_TalonRX rightFront;
+  WPI_TalonRX rightBack;
+
+  DifferentialDrive drive;
+
+  /** Creates a new DrivetrainSubsystem. */
+  public DrivetrainSubsystem() {
+
+    leftFront = new WPI_TalonRX(1);
+    leftBack = new WPI_TalonRX(2);
+    rightFront = new WPI_TalonRX(3);
+    rightBack = new WPI_TalonRX(4);
+
+    leftFront.setInverted(true);
+
+    leftBack.follow(leftFront);
+    rightBack.follow(rightFront);
+
+    drive = new DifferentialDrive(leftFront,rightFront);
+  }
+
+  public void setSpeedTank(double leftSpeed, double rightSpeed){
+    drive.tankDrive(leftSpeed,rightSpeed);
+  }
+
+  public void setSpeedArcade(double speed, double rotation){
+    drive.arcadeDrive(speed,rotation);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
